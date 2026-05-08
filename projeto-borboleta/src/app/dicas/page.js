4e9388e3619ac99cb41f00d186d3082b5d0cd52e
Luan Souza <luans0uzacost@gmail.com>
2026-05-08 00:38:07 +0000

    Adiciona rotas simples no Next.js

--- projeto-borboleta/src/app/dicas/page.js
+++ projeto-borboleta/src/app/dicas/page.js
@@ -106,6 +106,17 @@ export default function Dicas() {
           ))}
         </section>
       </main>
+      <footer>
+        <p>Entre em contato</p>
+        <p>Nossas redes</p>
+        <p>Suporte</p>
+
+        <nav id="footer-nav">
+          <a href="#">Instagram</a>
+          <a href="#">LinkedIn</a>
+          <a href="#">Contato</a>
+        </nav>
+      </footer>
     </>
   );
 }
